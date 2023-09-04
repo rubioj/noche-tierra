3e855ce46dddaaf34101309b23a75a620211dbdd
Juan Rubio <rubiojuan@gmail.com>
2023-09-03 19:30:47 -0700

    use email entered

--- send-mail.php
+++ send-mail.php
@@ -1,18 +1,27 @@
 <?php
+
+<?php
+// For debugging: print all POST data
+echo "<pre>";
+print_r($_POST);
+echo "</pre>";
+
+// Rest of your PHP script...
+
 // Check if the request method is post
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     $userName = $_POST['userName'];
-  $userEmail = $_POST['userEmail'];
+    $userEmail = $_POST['userEmail'];
 
     // Recipient email
     $to = $userEmail;
 
     // Subject
-    $subject = 'Welcome';
+    $subject = 'Welcome' . $userName;
 
     // Message
-    $message = 'We are very excited to have you be part of our art community';
+    $message = 'We are very excited you have joined our art community';
 
     // Additional headers
     $headers = 'From: info@juanrubio.us' . "\r\n" .
